dbe725b2b539c2ee4880224520090c32432290c9
seniorProjectUse2019 <seniorprojectice2019@gmail.com>
2019-04-15 19:33:01 +0700

    new one

--- Condo/settings.py
+++ Condo/settings.py
@@ -13,7 +13,7 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 import os
 import django_heroku
 import dj_database_url
-from decouple import config
+
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config('SECRET_KEY')
+SECRET_KEY = '8ihps+4sx)i+5%&@-swwm^g^ldb!vz^58da)3df0w$loz*9k#k'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
